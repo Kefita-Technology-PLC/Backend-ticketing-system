6dd6a22a2e64ff651937ed79a169d08c866b3b7b
Biruk Lemma <biruklemmadebela@gmail.com>
2024-08-20 21:04:58 +0300

    tarrif fake data generation

--- database/factories/TariffFactory.php
+++ database/factories/TariffFactory.php
@@ -20,13 +20,16 @@ class TariffFactory extends Factory
     {
         return [
             'station_id' => Station::factory()->create(),
-            'destination_id' => Destination::factory()->create(),
+            // 'destination_id' => Destination::factory()->create(),
             'distance'=> fake()->randomElement(['4kms', '1.5kms','2kms', '10kms', '7kms']),
-            'number_of_passengers' => fake()->randomElement(['12', '24', '20', '10', '5', '8', '9']),
+            'destination' => fake()->randomElement(['mexico','megenagna','cmc','goro','taffo', 'kara', 'wesen', 'urael', 'bole', 'wollo-sefer', 'atlas', '22-mazoriya', 'saris', 'lebu', 'kotebe']),
+            'origin' => fake()->randomElement(['mexico','megenagna','cmc','goro','taffo', 'kara', 'wesen', 'urael', 'bole', 'wollo-sefer', 'atlas', '22-mazoriya', 'saris', 'lebu', 'kotebe']),
+            'car_type' => fake()->randomNumber(['lochin', 'mini-bus', 'automotive']),
+            // 'number_of_passengers' => fake()->randomElement(['12', '24', '20', '10', '5', '8', '9']),
             'level1_price' => fake()->randomNumber(3),
             'level2_price' => fake()->randomNumber(3),
             'level3_price' => fake()->randomNumber(3),
-            'total_triff' => fake()->randomNumber(4)
+            // 'total_triff' => fake()->randomNumber(4)
         ];
     }
 }
