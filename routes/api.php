f7087f8aaed4262714c20161956f8a3317296366
Biruk Lemma <biruklemmadebela@gmail.com>
2024-08-21 15:12:31 +0300

    compilation errors

--- routes/api.php
+++ routes/api.php
@@ -58,8 +58,6 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
 
     Route::get('/general-report', [ReportController::class, 'generalReport'])->middleware(['role:admin']);
 
-
-
     Route::get('/monthly-report', [ReportController::class, 'monthlyReport'])->middleware(['role:admin']);
 
     Route::get('/yearly-report', [ReportController::class, 'yearlyReport'])->middleware(['role:admin']);
