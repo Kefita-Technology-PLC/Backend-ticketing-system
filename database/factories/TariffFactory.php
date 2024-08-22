88517fe314cc37d11c729c87134c42994105f91e
Biruk Lemma <biruklemmadebela@gmail.com>
2024-08-22 20:02:19 +0300

    added fake tariff factories

--- database/factories/TariffFactory.php
+++ database/factories/TariffFactory.php
@@ -22,7 +22,7 @@ class TariffFactory extends Factory
             'station_id' => Station::factory()->create(),
             // 'destination_id' => Destination::factory()->create(),
             'distance'=> fake()->randomElement(['4kms', '1.5kms','2kms', '10kms', '7kms']),
-            'destination' => fake()->randomElement(['mexico','megenagna','cmc','goro','taffo', 'kara', 'wesen', 'urael', 'bole', 'wollo-sefer', 'atlas', '22-mazoriya', 'saris', 'lebu', 'kotebe']),
+            'destination' => fake()->randomElement(['mexico','megenagna','cmc','goro','taffo', 'kara', 'wesen', 'ural', 'bole', 'wollo-sefer', 'atlas', 'kazanchis','piassa', '22-mazoriya', 'saris', 'lebu', 'kotebe']),
             'origin' => fake()->randomElement(['mexico','megenagna','cmc','goro','taffo', 'kara', 'wesen', 'urael', 'bole', 'wollo-sefer', 'atlas', '22-mazoriya', 'saris', 'lebu', 'kotebe']),
             'car_type' => fake()->randomNumber(['lochin', 'mini-bus', 'automotive']),
             // 'number_of_passengers' => fake()->randomElement(['12', '24', '20', '10', '5', '8', '9']),
